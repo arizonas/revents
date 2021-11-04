d157180cbded8e697ce9399086e607cba442ba0d
sinycop <sinycop@gmail.com>
2021-11-04 14:00:28 +0700

    End of section 1

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app/layout/App';
+import './app/layout/styles.css'
+
+const rootEl = document.getElementById('root');
+
+function render() {
+  ReactDOM.render(<App />, rootEl)
+}
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', function () {
+    setTimeout(render);
+  })
+}
+
+render();
